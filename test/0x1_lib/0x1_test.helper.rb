0d5ee277936c378be27c21f2a911bd2f27ebbb95
pima <pima@3eclipses.com>
2012-09-30 21:11:04 +0200

    test first pre-setup

---
+++ test/0x1_lib/0x1_test.helper.rb
@@ -0,0 +1,16 @@
+# encoding: utf-8
+
+module X module Lib module Dev
+
+
+end end end
+
+# ____________________________________________________________________
+# >>>>>  projet epiculture/ec1   >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>#{{{
+# Sources, Infos & Contact : http://www.epiculture.org
+# Author: Pierre-Maël Crétinon
+# License: GNU GPLv3 ( www.epiculture.org/ec1/LICENSE )
+# Copyright: 2010-2012 Pierre-Maël Crétinon
+# Sponsor: studio Helianova - http://studio.helianova.com
+# ――――――――――――――――――――――――――――――――――――――#}}}
+# vim:
